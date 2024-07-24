213d4499623e1913f6498a87d7fe4dfa223c0232
premgc2025@gmail.com <premgc2025@gmail.com>
2024-07-24 17:20:19 +0545

    Added API usl

--- helper.js
+++ helper.js
@@ -1,3 +1,4 @@
 
-export const baseurl = "http://localhost:5000";
+// export const baseurl = "http://localhost:5000";
 // export const baseurl = "https://mernbackend-8x3t.onrender.com"
+export const baseurl = "https://api-mern-2.onrender.com"
